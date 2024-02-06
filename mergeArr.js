5378f82180b54a9eb6772677f4c39aa3c9ae5974
AmitKumar2136 <132092023+AmitKumar2136@users.noreply.github.com>
2024-02-06 19:27:14 +0530

    Update mergeArr.js

--- mergeArr.js
+++ mergeArr.js
@@ -20,3 +20,40 @@ const arr1 = [ 10, 20, 30, 45, 50];
 const arr2 = [ 15, 25, 26, 35];
 const output = mergeArr(arr1, arr2);
 console.log(output);
+
+
+
+function mergeSortedArrays(arr1, arr2) {
+    let mergedArray = [];
+    let i = 0;
+    let j = 0;
+    
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            mergedArray.push(arr1[i]);
+            i++;
+        } else {
+            mergedArray.push(arr2[j]);
+            j++;
+        }
+    }
+
+    while (i < arr1.length) {
+        mergedArray.push(arr1[i]);
+        i++;
+    }
+
+    while (j < arr2.length) {
+        mergedArray.push(arr2[j]);
+        j++;
+    }
+
+    return mergedArray;
+}
+
+let arr1 = [10, 20, 30, 45, 50];
+let arr2 = [15, 25, 26, 35];
+
+let mergedArray = mergeSortedArrays(arr1, arr2);
+console.log(mergedArray);
+
